123331100972de0429bf77a7bb7ea956ccfbc140
hyunwoo00 <khwland090@gmail.com>
2024-08-24 12:02:14 +0900

    fianl commit
    + portal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-
-import Player from './components/Player';
-import TimerChallenge from './components/TimerChallenge';
+import Player from "./components/Player";
+import TimerChallenge from "./components/TimerChallenge";
 
 function App() {
   return (
     <div>
-      <Player/>
-      <div id='challenges'>
-        <TimerChallenge level="Easy" targetTime={1}/>
-        <TimerChallenge level="Not Easy" targetTime={5}/>
-        <TimerChallenge level="Getting tough" targetTime={10}/>
-        <TimerChallenge level="Pros only" targetTime={15}/>
+      <Player />
+      <div id="challenges">
+        <TimerChallenge level="Easy" targetTime={3} />
+        <TimerChallenge level="Not Easy" targetTime={5} />
+        <TimerChallenge level="Getting tough" targetTime={10} />
+        <TimerChallenge level="Pros only" targetTime={15} />
       </div>
     </div>
   );
